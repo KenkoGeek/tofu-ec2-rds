d6f02d9820258c5afc192a7adf4e7684afce770a
Franklin G. Mendoza <franklin.g.mendoza@gmail.com>
2026-03-17 19:10:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,10 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "demo-vpc" }
+  tags = { 
+    Name = "demo-vpc"
+    manual = "true"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
